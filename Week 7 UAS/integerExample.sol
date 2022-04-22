47d97b2efde7c4b93502ba39b030ef1b53a47eeb
Agung Panjimasjaya <69679429+apanji404@users.noreply.github.com>
2022-04-22 23:16:13 +0700

    Delete integerExample.sol

--- Week 7 UAS/integerExample.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.1;
-
-contract integerExample {
-    uint public myUint;
-    function setMyUint(uint _myUint) public {
-        myUint = _myUint;
-        }
-}
