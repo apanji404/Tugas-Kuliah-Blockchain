edc8cb5d557e6f07784f57054708213395730eec
Agung Panjimasjaya <69679429+apanji404@users.noreply.github.com>
2022-04-22 23:16:01 +0700

    Delete booleanExample.sol

--- Week 7 UAS/booleanExample.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.1;
-
-contract booleanExample {
-    bool public myBool;
-    function setMyBool(bool _myBool) public {
-        myBool = _myBool;
-        }
-}
